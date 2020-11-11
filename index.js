c05402081d12df5720c4998698d7f3d0f131efd1
taratimmerman <timmermantara@gmail.com>
2020-11-11 16:42:56 -0700

    24 passing, 3 failing(STRETCH)

--- index.js
+++ index.js
@@ -114,9 +114,22 @@ console.log(carOne.tank);
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+const babyOne = new Baby('Jack Jack', 1, 'Rocky');
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+}
+
+babyOne.play();
+
 
 /* 
   TASK 4
